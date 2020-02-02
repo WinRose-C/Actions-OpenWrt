f2647b0c9b79d865d5bb17d41b00bf498c433387
WinRose <60416631+WinRose-C@users.noreply.github.com>
2020-02-02 19:04:43 +0800

    Update diy.sh
    修改IP为192.168.2.1

--- diy.sh
+++ diy.sh
@@ -6,4 +6,7 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+# Delete default password
+#sed -i 's/V4UetPzk$CYXluq4wUazHjmCDBCqXF./reD9PyMM$AAqUHZiyzB6KFuEiRiVu91/g' package/lean/default-settings/files/zzz-default-settings
+sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-default-settings
